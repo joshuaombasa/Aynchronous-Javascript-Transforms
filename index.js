3c327f76f3d60714588959307855fd31ebf4a409
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-04 15:52:35 +0300

    added script

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 let approval = 'Not Approved'
 const approvalStatus = document.getElementById("approval-status")
 
-function getApproval(callback) {
-    setTimeout(() => {
-        approval = 'Approved'
-        callback()
-    },2000)
-}
+// function getApproval(callback) {
+//     setTimeout(() => {
+//         approval = 'Approved'
+//         callback()
+//     },2000)
+// }
 
 getApproval(() => {
     approvalStatus.textContent = approval
 })
 
 approvalStatus.textContent = approval
+
+
+function getApproval() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Approved')
+        })
+    })
+}
